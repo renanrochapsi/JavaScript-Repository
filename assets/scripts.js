ecad0de5b3483ab4ddf20f84fc7074e074b6ab2d
renanrochapsi <137223088+renanrochapsi@users.noreply.github.com>
2023-09-15 15:08:34 -0300

    Add files via upload

---
+++ assets/scripts.js
@@ -0,0 +1,11 @@
+/*
+q a página padrão será a foto da luz apagada
+fazer um botão que irá mudar a imagem para a
+foto da luz acesa 
+*/
+
+const lampadaApagada = document.querySelector('.container');
+lampadaApagada.addEventListener('click', function(e){
+    e.preventDefault;
+lampadaApagada.innerHTML()
+});
